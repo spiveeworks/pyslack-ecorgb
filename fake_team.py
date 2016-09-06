1d81515a07cb5c85253ab46d2c8ba5faad8bf1f0
Spivee <spiveehere@gmail.com>
2016-09-06 14:37:00 +1000

    Specializing branches - removing redundancy

--- fake_team.py
+++
@@ -1,8 +0,0 @@
-channel_info = [{'name': name, 'id': id} for id, name in enumerate(
-    "red blue".split()
-)]
-
-user_info = [{'name': 'player', 'id': 0} for id, name in enumerate(
-    "awesome_guy boring_guy".split()
-)]
-
